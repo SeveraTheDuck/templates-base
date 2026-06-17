042dfceefef8988800db084f4df02d76c68ac892
SeveraTheDuck <severa.dev@gmail.com>
2026-06-17 15:58:11 +0300

    chore: checkouts out of actions, nix self system fix

--- template/nix/base/checks.nix
+++ template/nix/base/checks.nix
@@ -2,16 +2,16 @@
 # treefmt registers its own `checks.treefmt`. Add spelling and licensing.
 # perSystem module.
 
-{ self, pkgs, ... }:
+{ inputs, pkgs, ... }:
 {
   checks = {
     spelling = pkgs.runCommand "typos" { } ''
-      ${pkgs.typos}/bin/typos ${self}
+      ${pkgs.typos}/bin/typos ${inputs.self}
       touch $out
     '';
 
     reuse = pkgs.runCommand "reuse" { } ''
-      ${pkgs.reuse}/bin/reuse --root ${self} lint
+      ${pkgs.reuse}/bin/reuse --root ${inputs.self} lint
       touch $out
     '';
   };
